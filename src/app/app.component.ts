f45ea19c5230db71dbbb50b999622edb1292ad32
ShreyasGS <commute.application@gmail.com>
2020-07-05 23:11:46 +0530

    First Time

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'empowerus';
+  options: AnimationOptions = {
+    path: 'https://assets2.lottiefiles.com/private_files/lf30_g4ft9Z.json',
+  };
+ 
+  animationCreated(animationItem: AnimationItem): void {
+    console.log(animationItem);
+  }
+}
